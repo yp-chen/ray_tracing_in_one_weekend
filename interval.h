9c7b7b20fd2238793af64a116cc1cb466158fd18
Yongpeng Chen <yongpeng.chen@momenta.ai>
2024-08-12 14:40:50 +0800

    add interval

---
+++ interval.h
@@ -0,0 +1,22 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+#include "global.h"
+//区间类
+class Interval {
+    public:
+        Interval() : m_start_(MY_INFINITY), m_end_(-MY_INFINITY) {}
+        Interval(double a, double b) : m_start_(a), m_end_(b) {}
+        double start() const { return m_start_; }
+        double end() const { return m_end_; }
+        bool contains(double val) const { return val >= m_start_ && val <= m_end_; } //判断x是否在闭区间内
+        bool surrounds(double val) const { return val > m_start_ && val < m_end_;} //判断x是否在开区间内
+
+        static const Interval Empty;
+        static const Interval Universe;
+    private:
+        double m_start_;
+        double m_end_;
+};
+const Interval Interval::Empty = Interval(MY_INFINITY, -MY_INFINITY);
+const Interval Interval::Universe = Interval(-MY_INFINITY, MY_INFINITY);
+#endif
